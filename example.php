cb53d38677bde84cf00844c9d0e2caee343349ac
ReduxRedstone <halolink44@gmail.com>
2016-05-07 13:04:19 -0400

    added name history

--- example.php
+++ example.php
@@ -9,3 +9,9 @@ echo $uuid."<br>";
 
 $username = $player->getUsername("997e3b3a-cd4b-4334-9a96-4c4cd51decf8");
 print_r($username);
+
+$usernameHist1 = $player->nameHist("997e3b3a-cd4b-4334-9a96-4c4cd51decf8");
+print_r($usernameHist1);
+
+$usernameHist = $player->nameHist("ReduxRedstone");
+print_r($usernameHist);
